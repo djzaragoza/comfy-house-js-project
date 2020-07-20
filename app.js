a6d551816a8d0928bd692174f44ee0c2c2072117
unknown <dj.zaragoza@icloud.com>
2020-07-20 01:34:46 -0700

    working on functionality

--- app.js
+++ app.js
@@ -15,7 +15,22 @@ let cart = []; // empty array for cartBtn
 
 // getting products 
 class Products {
-
+   async getProducts() {
+      try {
+         let result = await fetch('products.json')
+         let data = await result.json();
+         let products = data.items;
+         products = products.map(item => {
+            const { title, price } = item.fields;
+            const { id } = item.sys
+            const image = item.fields.image.fields.file.url;
+            return { title, price, id, image }
+         })
+         return products
+      } catch (error) {
+         console.log(error);
+      }
+   }
 }
 
 // display products
@@ -31,6 +46,9 @@ class Storage {
 document.addEventListener('DOMContentLoaded', () => {
    const ui = new UI()
    const products = new Products();
+
+   // get all products
+   products.getProducts().then(data => console.log(data));
 });
 
 
